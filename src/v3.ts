d60e9a94dc20de8dadd62db595aa041ffef6d19c
-akku- <151517253+akku1139@users.noreply.github.com>
2024-03-14 17:21:30 +0900

    ざつなじっそう

--- src/v3.ts
+++ src/v3.ts
@@ -1,12 +1,12 @@
 import type { Route } from "./types.ts";
 import { HTTPStatus, baseResponse, error } from "./utils.ts";
 
-const v3: Route = async (r) => {
-  if(r.method === "OPTIONS") {
+const v3: Route = async (req) => {
+  if(req.method === "OPTIONS") {
     return baseResponse(undefined, { status: HTTPStatus.OK });
   }
 
-  const isSocket = r.headers.get("upgrade");
+  const isSocket = req.headers.get("upgrade");
   if (isSocket) {
     return error(
       new Error("Unimplemented"),
@@ -15,6 +15,25 @@ const v3: Route = async (r) => {
       500
     );
   }
+
+  const res = await fetch(
+    req.headers.get("X-Bare-URL"),
+    {
+      method: req.method,
+      headers: new Headers(req.headers.get("X-Bare-Headers"))
+    }
+  );
+
+  return baseResponse( res.body , {
+    headers: {
+      "Content-Encoding": res.headers.get("Content-Encoding"),
+      "Content-Length": res.headers.get("Content-Length"),
+      "Content-Type": res.headers.get("Content-Type"),
+      "X-Bare-Headers": JSON.stringify(res.headers),
+      "X-Bare-Status": res.status,
+      "X-Bare-Status-Text": res.statusText,
+    },
+  });
 };
 
 export default v3;
