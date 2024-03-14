47996a4740378bed3418fa62b0a556bbad83f70c
-akku- <151517253+akku1139@users.noreply.github.com>
2024-03-14 16:47:23 +0900

    v3ことはじめ

--- src/v3.ts
+++ src/v3.ts
@@ -1,5 +1,20 @@
 import type { Route } from "./types.ts";
+import { HTTPStatus, baseResponse, error } from "./utils.ts";
 
-const v3: Route = async (r) => {};
+const v3: Route = async (r) => {
+  if(r.method === "OPTIONS") {
+    return baseResponse(undefined, { status: HTTPStatus.OK });
+  }
+
+  const isSocket = r.headers.get("upgrade");
+  if (isSocket) {
+    return error(
+      new Error("Unimplemented"),
+      "UNKNOWN",
+      "unknown",
+      500
+    );
+  }
+};
 
 export default v3;
